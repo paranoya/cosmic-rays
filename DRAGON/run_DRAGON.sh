f7cef3e4b5cddc338f73c3cfcf5b5793042f3f67
Yago Ascasibar <yago.ascasibar@uam.es>
2023-07-25 17:55:09 +0800

    First succesful DRAGON run

--- DRAGON/run_DRAGON.sh
+++ DRAGON/run_DRAGON.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 export DRAGON_DIR=/home/yago/public/git/DRAGON2-Beta_version
-export WORKING_DIR=$(PWD)
-export MODEL=run_2D
+export WORKING_DIR=$(pwd)
+export MODEL=run_2D_DM
 
 cd $DRAGON_DIR
-echo ./DRAGON $WORKING_DIR/input/$MODEL.xml
+cp $WORKING_DIR/output/$MODEL* $WORKING_DIR/output/bak/
+./DRAGON $WORKING_DIR/input/$MODEL.xml &> $WORKING_DIR/output/$MODEL\_output.txt
+cp output/$MODEL* $WORKING_DIR/output/
 
 : <<'Bye'
                                                          ... Paranoy@ rulz! ;^D
