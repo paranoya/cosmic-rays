ea63719146da0ee48575c7743e52b2844b90f8bf
Yago Ascasibar <yago.ascasibar@uam.es>
2023-07-25 17:37:44 +0800

    Added first DRAGON scripts

---
+++ DRAGON/run_DRAGON.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export DRAGON_DIR=/home/yago/public/git/DRAGON2-Beta_version
+export WORKING_DIR=$(PWD)
+export MODEL=run_2D
+
+cd $DRAGON_DIR
+echo ./DRAGON $WORKING_DIR/input/$MODEL.xml
+
+: <<'Bye'
+                                                         ... Paranoy@ rulz! ;^D
+Bye
+
