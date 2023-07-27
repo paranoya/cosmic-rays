f1b110018ec31c7c7d0eadae94c2b714356653b6
Yago Ascasibar <yago.ascasibar@uam.es>
2023-07-28 07:47:43 +0800

    Run model grid

--- DRAGON/run_DRAGON.sh
+++ DRAGON/run_DRAGON.sh
@@ -2,12 +2,20 @@
 
 export DRAGON_DIR=/home/yago/public/git/DRAGON2-Beta_version
 export WORKING_DIR=$(pwd)
-export MODEL=run_2D_DM
+
+if [[ $# != 1 ]];
+then
+    echo "USAGE: run_DRAGON <model_name>"
+    exit
+fi
 
 cd $DRAGON_DIR
-cp $WORKING_DIR/output/$MODEL* $WORKING_DIR/output/bak/
-./DRAGON $WORKING_DIR/input/$MODEL.xml &> $WORKING_DIR/output/$MODEL\_output.txt
-cp output/$MODEL* $WORKING_DIR/output/
+for MODEL in $@
+do
+    [ -f "$WORKING_DIR/output/$MODEL.fits.gz" ] && cp $WORKING_DIR/output/$MODEL* $WORKING_DIR/output/bak/
+    ./DRAGON $WORKING_DIR/input/$MODEL.xml &> $WORKING_DIR/output/$MODEL\_output.txt
+    [ -f "output/$MODEL.fits.gz" ] && cp output/$MODEL* $WORKING_DIR/output/
+done
 
 : <<'Bye'
                                                          ... Paranoy@ rulz! ;^D
---
+++ DRAGON/run_model_grid.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Generate a new DRAGON model, changing some of the parameters.
+
+Created on July 28, 2023
+@author: Yago Ascasibar
+"""
+
+from __future__ import print_function, division
+
+import os
+import xml.dom.minidom as dom
+#import subprocess  # TODO: use subprsubprocess.run() it instead of os.system()
+import numpy as np
+
+
+def generate_model(base_model_name, suffix, change_params):
+    '''
+    Read `base_model_name`.xml,
+    change the parameters in the dictionary `change_params` to the specified values,
+    and save as `base_model_name`_`suffix`.xml
+    '''
+    
+    # read original XML file
+    base_model_path = os.path.join('input', base_model_name)
+    with open(f'{base_model_path}.xml', 'r') as file:
+        lines = file.readlines()
+    base_model_str = ''.join([lines[0], '<DRAGONmodel>\n'] + lines[1:] + ['</DRAGONmodel>'])  # proper XML can only contain one root node
+    model = dom.parseString(base_model_str)
+
+    # change parameters
+    for param in change_params:
+        model.getElementsByTagName(param)[0].setAttribute('value', f'{change_params[param]}')
+
+    # write modified XML file
+    model_str = '\n'.join([lines[0]] + model.toxml().split('\n')[1:-1])
+    with open(f'{base_model_path}_{suffix}.xml', 'w') as file:
+        file.write(model_str)
+
+
+base_model_name = 'run_2D_DM'
+model_grid = {'D0_1e28': np.linspace(.5, 5, 3)}
+models_run = 0
+for param in model_grid:
+    print(f'Changing {param}:')
+    for value in model_grid[param]:
+        generate_model(base_model_name, models_run, {param: value})
+        print(f'- {param} = {value}: {base_model_name}_{models_run}')
+        os.system(f'./run_DRAGON.sh {base_model_name}_{models_run}')
+        models_run += 1
+print('                                        ... Paranoy@ Rulz!')
